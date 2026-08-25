20a52bf081cc872e140c9d75d2f1c15c336a5a00
nuko <p@nuko.tw>
2026-08-25 20:10:09 +0800

    ci: fix SDK version and make create-release resilient to publish iOS IPA

--- hentai_cosplay_downloader/android/app/build.gradle.kts
+++ hentai_cosplay_downloader/android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.hentaicosplay.hentai_cosplay_downloader"
-    compileSdk = 36
+    compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
@@ -18,7 +18,7 @@ android {
     defaultConfig {
         applicationId = "com.hentaicosplay.hentai_cosplay_downloader"
         minSdk = flutter.minSdkVersion
-        targetSdk = 36
+        targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
