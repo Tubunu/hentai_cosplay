9ce34b9712a63ed660c7c24cf8c75b3f36fa268e
nuko <p@nuko.tw>
2026-09-21 18:25:38 +0800

    ci: upgrade Android CI to Java 21, install SDK 36, and prioritize official maven repos

--- hentai_cosplay_downloader/android/build.gradle.kts
+++ hentai_cosplay_downloader/android/build.gradle.kts
@@ -1,12 +1,12 @@
 allprojects {
     repositories {
+        google()
+        mavenCentral()
         maven { url = uri("https://maven.aliyun.com/repository/google") }
         maven { url = uri("https://maven.aliyun.com/repository/public") }
         maven { url = uri("https://maven.aliyun.com/repository/gradle-plugin") }
         maven { url = uri("https://maven.aliyun.com/repository/central") }
         maven { url = uri("https://storage.flutter-io.cn/download.flutter.io") }
-        google()
-        mavenCentral()
     }
 }
 
--- hentai_cosplay_downloader/android/settings.gradle.kts
+++ hentai_cosplay_downloader/android/settings.gradle.kts
@@ -14,14 +14,14 @@ pluginManagement {
     includeBuild("$flutterSdkPath/packages/flutter_tools/gradle")
 
     repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
         maven { url = uri("https://maven.aliyun.com/repository/google") }
         maven { url = uri("https://maven.aliyun.com/repository/public") }
         maven { url = uri("https://maven.aliyun.com/repository/gradle-plugin") }
         maven { url = uri("https://maven.aliyun.com/repository/central") }
         maven { url = uri("https://storage.flutter-io.cn/download.flutter.io") }
-        google()
-        mavenCentral()
-        gradlePluginPortal()
     }
 }
 
